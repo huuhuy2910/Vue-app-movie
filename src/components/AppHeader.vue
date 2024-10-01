d07060d6190ffc73022d622edda113b6ddd68639
huuhuy2910 <131759541+huuhuy2910@users.noreply.github.com>
2024-10-01 17:18:48 +0700

    Update AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,8 +4,11 @@
           <a class="navbar-brand" href="/">
               <img src="@/assets/logo.png" alt="Logo" width="100" height="auto" />
           </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
           <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav ms-auto"> <!-- Thêm ms-auto để đẩy navbarNav sang phải -->
+              <ul class="navbar-nav ms-auto">
                   <li class="nav-item">
                       <a class="nav-link active" aria-current="page" href="/">Trang Chủ</a>
                   </li>
@@ -28,5 +31,10 @@ export default {
 </script>
 
 <style>
-/* Nếu cần, bạn có thể thêm CSS tùy chỉnh cho header ở đây */
+@media (max-width: 768px) {
+    .navbar {
+        padding-left: 10px !important;
+        padding-right: 10px !important;
+    }
+}
 </style>
